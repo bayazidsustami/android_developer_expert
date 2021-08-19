0781b3f60af31bb2fd74403d359887f1be53ba51
bayazid_sustami <bayazidsustamy@gmail.com>
2021-08-19 20:23:56 +0800

    compete reactive search using flow

--- course/simple_reactive_flow/app/src/main/java/com/dicoding/course/myreactivesearch/MainActivity.kt
+++ course/simple_reactive_flow/app/src/main/java/com/dicoding/course/myreactivesearch/MainActivity.kt
@@ -2,10 +2,40 @@ package com.dicoding.course.myreactivesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
+@ExperimentalCoroutinesApi
+@FlowPreview
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edPlace = findViewById<AutoCompleteTextView>(R.id.ed_place)
+
+        edPlace.doOnTextChanged { text, _, _, _ ->
+            lifecycleScope.launchWhenCreated {
+                viewModel.queryChannel.send(text.toString())
+            }
+        }
+
+        viewModel.searchResult.observe(this){result ->
+            val placesName = arrayListOf<String>()
+            result.map {
+                placesName.add(it.placeName)
+            }
+            val adapter = ArrayAdapter(this, android.R.layout.select_dialog_item, placesName)
+            adapter.notifyDataSetChanged()
+            edPlace.setAdapter(adapter)
+        }
     }
 }
