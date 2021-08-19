414705e0cc820aaa459c2ba3c3fb41388aa71e06
bayazid_sustami <bayazidsustamy@gmail.com>
2021-08-19 21:25:58 +0800

    create example koin project

---
+++ course/simple_login_koin/app/src/main/java/com/dicoding/course/simplelogin/HomeActivity.kt
@@ -0,0 +1,32 @@
+package com.dicoding.course.simplelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.course.simplelogin.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    lateinit var userRepository: UserRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sesi = SessionManager(this)
+        userRepository = UserRepository.getInstance(sesi)
+
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
+
+        binding.btnLogout.setOnClickListener {
+            userRepository.logoutUser()
+            moveToMainActivity()
+        }
+    }
+
+    private fun moveToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
