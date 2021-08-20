2d5e3e5c4c75dca74cf632faea30c44e809a8063
bayazid_sustami <bayazidsustamy@gmail.com>
2021-08-20 09:50:35 +0800

    refactor project to use dagger2

--- course/starter-project/app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteFragment.kt
+++ course/starter-project/app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteFragment.kt
@@ -1,23 +1,34 @@
 package com.dicoding.tourismapp.favorite
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.tourismapp.App
 import com.dicoding.tourismapp.core.ui.TourismAdapter
+import com.dicoding.tourismapp.core.ui.ViewModelFactory
 import com.dicoding.tourismapp.databinding.FragmentFavoriteBinding
 import com.dicoding.tourismapp.detail.DetailTourismActivity
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import javax.inject.Inject
 
 class FavoriteFragment : Fragment() {
 
-    private val favoriteViewModel: FavoriteViewModel by viewModel()
+    @Inject
+    lateinit var factory: ViewModelFactory
+    private val favoriteViewModel: FavoriteViewModel by viewModels { factory }
 
     private var _binding: FragmentFavoriteBinding? = null
     private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().application as App).appComponent.inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
