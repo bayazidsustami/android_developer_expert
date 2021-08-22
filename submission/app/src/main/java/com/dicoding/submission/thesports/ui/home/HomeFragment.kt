be183ecaaab71f74d534fadacda26c9a30384f3c
bayazid_sustami <bayazidsustamy@gmail.com>
2021-08-22 22:56:28 +0800

    add adapter for list match

--- submission/app/src/main/java/com/dicoding/submission/thesports/ui/home/HomeFragment.kt
+++ submission/app/src/main/java/com/dicoding/submission/thesports/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.dicoding.submission.thesports.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.submission.thesports.commons.base.BaseFragment
+import com.dicoding.submission.thesports.commons.extensions.gone
+import com.dicoding.submission.thesports.commons.extensions.visible
 import com.dicoding.submission.thesports.core.data.commons.Resource
 import com.dicoding.submission.thesports.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,21 +15,47 @@ class HomeFragment: BaseFragment<FragmentHomeBinding>(
     FragmentHomeBinding::inflate
 ) {
     private val viewModel: HomeViewModel by viewModels()
+    private val adapter: EventAdapter by lazy { EventAdapter() }
 
     override fun onViewCreated(savedInstanceState: Bundle?) {
+        initAdapter()
+        observeResult()
+    }
+
+    private fun observeResult(){
         viewModel.listEvent.observe(viewLifecycleOwner){result ->
             when(result){
                 is Resource.Loading -> {
-                    Log.d("RESPONSE", "Loading")
+                    binding.pBar.visible()
+                    binding.rvListEvent.gone()
+                    binding.tvError.gone()
                 }
                 is Resource.Success -> {
                     val data = result.data
-                    Log.d("RESPONSE", data.toString())
+                    binding.pBar.gone()
+                    binding.rvListEvent.visible()
+                    binding.tvError.gone()
+                    if (data != null){
+                        adapter.submitList(data)
+                    }
                 }
                 is Resource.Error -> {
-                    Log.d("RESPONSE", "Error : ${result.message}")
+                    binding.pBar.gone()
+                    binding.rvListEvent.gone()
+                    binding.tvError.run {
+                        visible()
+                        text = result.message
+                    }
                 }
             }
+        }
+    }
+
+    private fun initAdapter(){
+        with(binding.rvListEvent){
+            adapter = this@HomeFragment.adapter
+            layoutManager = LinearLayoutManager(requireContext())
+            hasFixedSize()
         }
     }
 }
--- submission/favorite/src/main/java/com/dicoding/submission/thesports/favorite/FavoriteFragment.kt
+++ submission/favorite/src/main/java/com/dicoding/submission/thesports/favorite/FavoriteFragment.kt
@@ -1,13 +1,51 @@
 package com.dicoding.submission.thesports.favorite
 
+import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.submission.thesports.commons.base.BaseFragment
+import com.dicoding.submission.thesports.di.FavoriteModuleDependencies
+import com.dicoding.submission.thesports.favorite.component.DaggerFavoriteComponent
 import com.dicoding.submission.thesports.favorite.databinding.FragmentFavoriteBinding
+import com.dicoding.submission.thesports.favorite.viewModel.FavoriteViewModel
+import com.dicoding.submission.thesports.favorite.viewModel.FavoriteViewModelFactory
+import com.dicoding.submission.thesports.ui.home.EventAdapter
+import dagger.hilt.android.EntryPointAccessors
+import javax.inject.Inject
 
 class FavoriteFragment: BaseFragment<FragmentFavoriteBinding>(
     FragmentFavoriteBinding::inflate
 ) {
-    override fun onViewCreated(savedInstanceState: Bundle?) {
+    @Inject lateinit var factory: FavoriteViewModelFactory
 
+    private val viewModel: FavoriteViewModel by viewModels { factory }
+    private val adapter: EventAdapter by lazy { EventAdapter() }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        DaggerFavoriteComponent.builder()
+            .context(requireContext())
+            .appDependencies(
+                EntryPointAccessors.fromApplication(
+                    requireContext().applicationContext,
+                    FavoriteModuleDependencies::class.java
+                )
+            ).build().inject(this)
+    }
+
+    override fun onViewCreated(savedInstanceState: Bundle?) {
+        initAdapter()
+        viewModel.listFavoriteUseCase.observe(viewLifecycleOwner){result ->
+            adapter.submitList(result)
+        }
+    }
+
+    private fun initAdapter(){
+        with(binding.rvListEvent){
+            adapter = this@FavoriteFragment.adapter
+            layoutManager = LinearLayoutManager(requireContext())
+            hasFixedSize()
+        }
     }
 }
