ac0ffa0ac9e209831b9a59cc2805f16cdc4e0353
bayazid_sustami <bayazidsustamy@gmail.com>
2021-08-26 10:18:29 +0800

    implement ssl pinning

--- submission/core/src/main/java/com/dicoding/submission/thesports/core/di/NetworkModule.kt
+++ submission/core/src/main/java/com/dicoding/submission/thesports/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -20,10 +21,16 @@ class NetworkModule {
     @Singleton
     @Provides
     fun provideOkhttpClient(): OkHttpClient{
+        val hostName = "www.thesportsdb.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostName, "sha256/Yl6IYMBnxrwtn9DWo32vSutvY3gRn56iEfWj5/66c88=")
+            .add(hostName, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
